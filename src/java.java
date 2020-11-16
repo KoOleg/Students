f8b4a78b6686bd0cc4b2438bef1f4b30d4692297
Oleg <73615085+KoOleg@users.noreply.github.com>
2020-11-16 19:25:25 +0300

    Update java.java

--- src/java.java
+++ src/java.java
@@ -1,1 +1,9 @@
 
+public class ran {
+        public int getRandomInteger() {
+            Random random = new Random(); //генератор рандомных чисел
+            int result = random.nextInt(); //сгенерировать число
+            System.out.println(result);
+            return result; //возвращение результата
+        }
+    }
